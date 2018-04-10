7e526cf4ff1f1ac4970d8a89013f7d69e9c0eb16
Charlie Vieth <charlie.vieth@gmail.com>
2018-04-10 12:34:12 -0400

    Benchmark lager
    Initial benchmark result:
    
    goos: darwin
    goarch: amd64
    pkg: code.cloudfoundry.org/lager/benchmarks
    BenchmarkLogger_NoSink-8               	 3000000	       486 ns/op	     136 B/op	       6 allocs/op
    BenchmarkLogger-8                      	 1000000	      1385 ns/op	     704 B/op	      12 allocs/op
    BenchmarkLogger_Noop-8                 	 3000000	       510 ns/op	     136 B/op	       6 allocs/op
    BenchmarkLogger_Data-8                 	  500000	      2731 ns/op	    1688 B/op	      25 allocs/op
    BenchmarkLogger_Data_Error-8           	  500000	      3083 ns/op	    1784 B/op	      28 allocs/op
    BenchmarkLogger_WithData-8             	  500000	      2781 ns/op	    1680 B/op	      24 allocs/op
    BenchmarkLogger_CombineData-8          	  500000	      2777 ns/op	    1688 B/op	      25 allocs/op
    BenchmarkLogger_CombineData_Error-8    	  500000	      3102 ns/op	    1784 B/op	      28 allocs/op
    BenchmarkWriterSink_Small-8            	 1000000	      1662 ns/op	    1176 B/op	      15 allocs/op
    BenchmarkWriterSink_Large-8            	   50000	     40248 ns/op	   38472 B/op	      33 allocs/op
    BenchmarkWriterSink_Parallel_Small-8   	 1000000	      2056 ns/op	    1176 B/op	      15 allocs/op
    BenchmarkWriterSink_Parallel_Large-8   	   30000	     40777 ns/op	   38472 B/op	      33 allocs/op
    PASS
    ok  	code.cloudfoundry.org/lager/benchmarks	20.646s

---
+++ benchmarks/writer_sink_test.go
@@ -0,0 +1,204 @@
+package benchmarks
+
+import (
+	"fmt"
+	"io"
+	"runtime"
+	"testing"
+	"time"
+
+	"code.cloudfoundry.org/lager"
+)
+
+// TODO: Benchmark against a real file (maybe /dev/null) to demonstrate
+// the impact of calling Write() twice.
+
+type nopWriter struct{}
+
+func (nopWriter) Write(b []byte) (int, error) { return len(b), nil }
+
+// This should be a no-op
+func BenchmarkLogger_NoSink(b *testing.B) {
+	l := lager.NewLogger("benchmark")
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug")
+	}
+}
+
+func BenchmarkLogger(b *testing.B) {
+	l := lager.NewLogger("benchmark")
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug")
+	}
+}
+
+// When the log level is insufficient to trigger a write this should be a no-op
+func BenchmarkLogger_Noop(b *testing.B) {
+	l := lager.NewLogger("benchmark")
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.ERROR))
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug")
+	}
+}
+
+func BenchmarkLogger_Data(b *testing.B) {
+	data := lager.Data{
+		"id":     123456,
+		"method": "GET",
+		"url":    "https://golang.org/pkg/",
+	}
+	l := lager.NewLogger("benchmark")
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug", data)
+	}
+}
+
+// Highlight the impact of adding another value to the map after its
+// been constructed.
+func BenchmarkLogger_Data_Error(b *testing.B) {
+	data := lager.Data{
+		"id":     123456,
+		"method": "GET",
+		"url":    "https://golang.org/pkg/",
+	}
+	l := lager.NewLogger("benchmark")
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Error("error", io.EOF, data)
+	}
+}
+
+func BenchmarkLogger_WithData(b *testing.B) {
+	data := lager.Data{
+		"id":     123456,
+		"method": "GET",
+		"url":    "https://golang.org/pkg/",
+	}
+	l := lager.NewLogger("benchmark").WithData(data)
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug")
+	}
+}
+
+func BenchmarkLogger_CombineData(b *testing.B) {
+	data := lager.Data{
+		"id":     123456,
+		"method": "GET",
+		"url":    "https://golang.org/pkg/",
+	}
+	l := lager.NewLogger("benchmark").WithData(data)
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Debug("debug", data)
+	}
+}
+
+func BenchmarkLogger_CombineData_Error(b *testing.B) {
+	data := lager.Data{
+		"id":     123456,
+		"method": "GET",
+		"url":    "https://golang.org/pkg/",
+	}
+	l := lager.NewLogger("benchmark").WithData(data)
+	l.RegisterSink(lager.NewWriterSink(nopWriter{}, lager.DEBUG))
+	for i := 0; i < b.N; i++ {
+		l.Error("debug", io.EOF, data)
+	}
+}
+
+func benchmarkWriterSink(b *testing.B, log lager.LogFormat) {
+	sink := lager.NewWriterSink(nopWriter{}, lager.DEBUG)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.Log(log)
+	}
+}
+
+func BenchmarkWriterSink_Small(b *testing.B) {
+	log := lager.LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    "log-compenent",
+		Message:   "log-message",
+		LogLevel:  lager.DEBUG,
+		Data: lager.Data{
+			"url":    "https://golang.org/pkg/",
+			"method": "GET",
+		},
+	}
+	benchmarkWriterSink(b, log)
+}
+
+func BenchmarkWriterSink_Large(b *testing.B) {
+	var m runtime.MemStats // big struct
+	runtime.ReadMemStats(&m)
+
+	log := lager.LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    "log-compenent",
+		Message:   "log-message",
+		LogLevel:  lager.DEBUG,
+		Data: lager.Data{
+			"mem_stats": m,
+			"nested_data": lager.Data{
+				"id":     123456,
+				"method": "GET",
+				"url":    "https://golang.org/pkg/",
+			},
+		},
+	}
+	benchmarkWriterSink(b, log)
+}
+
+func BenchmarkWriterSink_Parallel_Small(b *testing.B) {
+	log := lager.LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    "log-compenent",
+		Message:   "log-message",
+		LogLevel:  lager.DEBUG,
+		Data: lager.Data{
+			"url":    "https://golang.org/pkg/",
+			"method": "GET",
+		},
+	}
+	sink := lager.NewWriterSink(nopWriter{}, lager.DEBUG)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			sink.Log(log)
+		}
+	})
+}
+
+func BenchmarkWriterSink_Parallel_Large(b *testing.B) {
+	var m runtime.MemStats // big struct
+	runtime.ReadMemStats(&m)
+
+	log := lager.LogFormat{
+		Timestamp: currentTimestamp(),
+		Source:    "log-compenent",
+		Message:   "log-message",
+		LogLevel:  lager.DEBUG,
+		Data: lager.Data{
+			"mem_stats": m,
+			"nested_data": lager.Data{
+				"id":     123456,
+				"method": "GET",
+				"url":    "https://golang.org/pkg/",
+			},
+		},
+	}
+	sink := lager.NewWriterSink(nopWriter{}, lager.DEBUG)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			sink.Log(log)
+		}
+	})
+}
+
+func currentTimestamp() string {
+	return fmt.Sprintf("%.9f", float64(time.Now().UnixNano())/1e9)
+}
